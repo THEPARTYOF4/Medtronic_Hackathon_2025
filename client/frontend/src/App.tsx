964ed0923535d7d6031876d175049940ef772409
rickeyjohnson <rdjwinter2004@gmail.com>
2025-11-07 21:43:46 -0500

    installed tailwind and vite

---
+++ client/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-500 text-white p-4 font-bold rounded-lg">
+        If this text is white with a red background, rounded corners, and bold font,
+        Tailwind CSS is working!
+      </div>
+    </>
+  )
+}
+
+export default App
